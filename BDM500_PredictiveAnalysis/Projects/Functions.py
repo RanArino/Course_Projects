7f2d3f3e4ee526f3074e6b7b34fd4535f853447e
Ran Arino <ranarino0611ca@gmail.com>
2023-10-26 23:54:06 -0400

    save 1026

--- BDM500_PredictiveAnalysis/Projects/Functions.py
+++ BDM500_PredictiveAnalysis/Projects/Functions.py
@@ -82,38 +82,50 @@ class Standardization:
 
         return new_features
     
-def gradient_descent(X, y, t, b, alpha=0.01, lambda_=0.5):
+def gradient_descent(X, y, t, s, eta=0.01, alpha=1, lambda_=0.5):
     """
     Return the following three matrix (dtype: np.array)
     - "theta"  -> parameters (intercept + coefficients) at each step
     - "y_hats" -> predicted values at each step
-    - "sigma"  -> prediction errors (actual - predicted values); SSE
+    - "error"  -> prediction errors (actual - predicted values); SSE
 
     Parameters:
     - "X": np.array -> independent variables
     - "y": np.array -> target variables
     - "t": int -> number of data that were used for the initial parameter creation.
-    - "b": int -> mini-batch sizes(b <= t) 
-    - "alpha": learning rate for gradient descent
-    - "lambda_": hyperparameter for the elastic net regularization.
+    - "s": int -> scope of the latest data for parameter updates 
+    - "eta": learning rate for gradient descent
+    - "alpha": how strength the regularization is.
+    - "lambda_": balancing between ridge and lasso regularization.
 
     Brief Steps:
-    - Initialize matries for theta, y_hats, sigma.
+    - Initialize matries for theta, y_hats, error.
     - Apply a given number of data ("t") to the mutiple linear regression (normal equation).
     - Define the initial parameters from the trained model.
     - At each step (total steps are len(y) - t):
         - Get a single pair of unfamilar data; both X and y.
         - Predict the target ("y_hats") based on the latest parameters("theta[i]").
-        - Calculate the difference between actual and predicted values; "sigma[i]".
-        - Update parameters for the next step ("theta[i+1]") by a given number of the errors;
-           if b=10, recalculating the predicted error based on the theta[i] and the latest 10 data.
+        - Calculate the difference between actual and predicted values; "error[i]".
+        - Update parameters for the next step ("theta[i+1]").
     """
+
     # define all matrix to be returned
     theta = np.zeros((len(y)-t + 1, 6))
     y_hats = np.zeros((len(y)-t, 1))
-    sigma = np.zeros((len(y)-t, 1))
+    error = np.zeros((len(y)-t, 1))
     # Modify the matrix of features; adding bias 
     X = np.insert(X[:], 0, 1, axis=1)
+
+    # define elastic net derivative
+    def elastic_net_der(theta, X, y, n=s, a_=alpha, l_=lambda_):
+        # reshape
+        X = X.reshape(n, -1)
+        y_hat = np.dot(X, theta).reshape(-1, 1)
+        # error weighted feature
+        ewf = 2/n * np.dot(X.T, y_hat - y).reshape(1, -1)
+        d_l1 = l_ * a_ * np.sign(theta)
+        d_l2 = (1 - l_) * a_ * theta
+        return ewf + d_l1 + d_l2
     
     # initial training
     init_X, init_y = X[0:t], y[0:t]
@@ -127,30 +139,23 @@ def gradient_descent(X, y, t, b, alpha=0.01, lambda_=0.5):
         # predicted variable
         y_hats[i] = np.dot(X_i, theta[i])
         # predicted error
-        sigma[i] =  (y_hats[i] - y_i)
-        # given number of predicted errros based on the current parameters
-        if b > 1:
-            # the given number (batch size; "b") of the latest X and y
-            X_b, y_b = X[idx-b+1:idx+1], y[idx-b+1:idx+1]  
-            # predicted error in the latest number "b" of data based on current parameters
-            sigmas = np.dot(X_b, theta[i].reshape(-1, 1)) - y_b
-            # calculate the mean of error weighted features (ewf_mu)
-            ewf_mu = np.mean(X_b * sigmas, axis=0)
+        error[i] =  (y_hats[i] - y_i)
+        # start gradient descent based on the data scope ("s")
+        # if scope is > 1, taking care of the predicted error from the recent data over a given scope ('s')
+        if s > 1:
+            # get the latest data based on the scope ('S')
+            X_, y_ = X[idx-s+1:idx+1], y[idx-s+1:idx+1]
+            derivative = elastic_net_der(theta[i], X_, y_)
+            theta[i+1] = theta[i] - eta * derivative
 
+        # if scope is 1, only taking care of the predicted error from the most recent data 
         else:
-            # if batch size is 1, just consider a predicted error based on the most recent data
-            ewf_mu = X_i * sigma[i]
-        
-        # get the partial derivative 
-        # pd = 2 / b * np.
-        # the gradient of the loss function with respect to the new observation 
-        gradient = 2 * (ewf_mu + (lambda_*alpha*np.sign(theta[i]) + 2*(1-lambda_)*alpha*theta[i]))
-        # update all parameters by stochastic gradient descent
-        theta[i+1] = theta[i] - alpha * gradient
+            derivative = elastic_net_der(theta[i], X_i, y_i)
+            theta[i+1] = theta[i] - eta * derivative
+            
+    return theta, y_hats, error
 
-    return theta, y_hats, sigma
-
-def evaluation(X, y, t, theta, y_hats, sigma):
+def evaluation(X, y, t, theta, y_hats, error):
     """
     Return the data frame; the following measureas by brackward eliminations:
     - Root Mean Square Error (rmse)
@@ -164,7 +169,7 @@ def evaluation(X, y, t, theta, y_hats, sigma):
     - "y": the actual target value (whole period)
     - "theta": all parameters (intercept & coefficients) at each step
     - "y_hats": the predicted target value at each step
-    - "sigma": difference between actual and predicted values at each step
+    - "error": difference between actual and predicted values at each step
 
     Requirement: len(y[t:]) == len(y_hats)
     """
@@ -186,9 +191,9 @@ def evaluation(X, y, t, theta, y_hats, sigma):
 
     #  number of coefficients
     for i in range(k+1):
-        # simply applying the given sigma
+        # simply applying the given error
         if i == 0:
-            sigma_ = sigma
+            error_ = error
         # conduct the backward elimination
         else:
             # copy the parameters matrix
@@ -198,11 +203,11 @@ def evaluation(X, y, t, theta, y_hats, sigma):
             # based on revised parameters, get the predicted value
             y_hats_ = np.sum(X_ * theta_, axis=1).reshape(-1, 1)
             # predicted error
-            sigma_ = y_ - y_hats_    
+            error_ = y_ - y_hats_    
     
         # Calculate Measures (rmse, se, r2, adj_r2, in order)
-        sse = np.sum(sigma_**2)
-        mm[i, 0] = np.sqrt((sigma_**2).mean()) 
+        sse = np.sum(error_**2)
+        mm[i, 0] = np.sqrt((error_**2).mean()) 
         mm[i, 1] = np.sqrt(sse / (n - k - 1))
         mm[i, 2] = 1 - sse/sst
         mm[i, 3] = 1 - (sse/(n - k -1))/(sst/(n-1))
@@ -215,7 +220,7 @@ def online_learning(t, X, y, alpha=0.01, lambda_=0.5):
     Return the following three matrix (dtype: np.array)
     - "theta"  -> parameters (intercept + coefficients) at each step
     - "y_hats" -> predicted values at each step
-    - "sigma"  -> prediction errors (actual - predicted values); SSE
+    - "error"  -> prediction errors (actual - predicted values); SSE
 
     Parameters:
     - "t": int -> number of months that were used for the initial training data.
@@ -225,18 +230,18 @@ def online_learning(t, X, y, alpha=0.01, lambda_=0.5):
     - "lambda_": hyperparameter for the elastic net regularization.
 
     Brief Steps:
-    - Initialize matries for theta, y_hats, sigma.
+    - Initialize matries for theta, y_hats, error.
     - Apply a given time ranges ("t") of the dataset to the mutiple linear regression.
     - Define the initial parameters from the trained model.
     - For each new data point (incremental learning):
         - Predict the target; "y_hats".
-        - Calculate the difference between actual and predicted values; "sigma"
+        - Calculate the difference between actual and predicted values; "error"
         - Update parameters by (stochastic) gradient descent with elastic regularization; "theta"
     """
     # define all matrix to be returned
     theta = np.zeros((len(y)-t + 1, 6))
     y_hats = np.zeros((len(y)-t, 1))
-    sigma = np.zeros((len(y)-t, 1))
+    error = np.zeros((len(y)-t, 1))
     # Modify the matrix of features; adding bias 
     X = np.insert(X[:], 0, 1, axis=1)
     
@@ -252,20 +257,20 @@ def online_learning(t, X, y, alpha=0.01, lambda_=0.5):
         # predicted variable
         y_hats[i] = np.dot(X_i, theta[i])
         # predicted error
-        sigma[i] =  (y_i - y_hats[i])
-        # the gradient of the loss function with respect to the new observation (normal: X_i * sigma[i])
-        gradient = -2 * X_i * sigma[i] + 2 * lambda_ * theta[i] + (1-lambda_) * np.sign(theta[i])
+        error[i] =  (y_i - y_hats[i])
+        # the gradient of the loss function with respect to the new observation (normal: X_i * error[i])
+        gradient = -2 * X_i * error[i] + 2 * lambda_ * theta[i] + (1-lambda_) * np.sign(theta[i])
         # update all parameters by stochastic gradient descent
         theta[i+1] = theta[i] - alpha * gradient
 
-    return theta, y_hats, sigma
+    return theta, y_hats, error
 
 def batch_learning(t, X, y, labmda_=0.5):
     """
     Return the following three matrix (dtype: np.array)
     - "theta"  -> parameters (intercept + coefficients) at each step
     - "y_hats" -> predicted values at each step
-    - "sigma"  -> prediction errors
+    - "error"  -> prediction errors
 
     Parameters:
     - "t": int -> number of months that were used for the initial training data.
@@ -274,17 +279,17 @@ def batch_learning(t, X, y, labmda_=0.5):
     - "lambda_": hyperparameter for the elastic net regularization.
 
     Brief Steps:
-    - Initialize matries for theta, y_hats, sigma.
+    - Initialize matries for theta, y_hats, error.
     - Train the model with a given time ranges ("t") of the dataset.
     - For each new data point (batch learning):
         - get all parameters (intercept and coefficients) by the least square method; "theta"
         - Predict the target value; "y_hats".
-        - Calculate the difference between actual and predicted values; "sigma"
+        - Calculate the difference between actual and predicted values; "error"
     """
     # define all matrix to be returned
     theta = np.zeros((len(y)-t + 1, 6))
     y_hats = np.zeros((len(y)-t, 1))
-    sigma = np.zeros((len(y)-t, 1))
+    error = np.zeros((len(y)-t, 1))
 
     # batch learning
     for i, idx in enumerate(range(t, len(y))):
@@ -296,11 +301,11 @@ def batch_learning(t, X, y, labmda_=0.5):
         X_new = np.append(np.array([1]), X[idx])
         y_hats[i] = np.dot(theta[i], X_new)
         # predicted error
-        sigma[i] = y[idx] - y_hats[i]
+        error[i] = y[idx] - y_hats[i]
 
     # trained the most recent data
     last = len(y)
     X_i, y_i = np.insert(X[last-t:last], 0, 1, axis=1) , y[last-t:last]
     theta[last-t] = np.linalg.inv(X_i.T.dot(X_i)).dot(X_i.T).dot(y_i).reshape(1,-1)
 
-    return theta, y_hats, sigma
+    return theta, y_hats, error
